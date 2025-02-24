3be43ab440fbf526f510db69d451bcd00e5c2a32
Lukari <lukari87@gmail.com>
2025-02-24 20:05:07 +0700

    main

--- Ex4_AddSubMulDiv_OnClick/app/src/main/java/tiil/edu/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_OnClick/app/src/main/java/tiil/edu/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -28,3 +28,66 @@ public class MainActivity extends AppCompatActivity {
         nutnhan = (Button) findViewById(R.id.btnnhan);
         nutchia = (Button) findViewById(R.id.btnchia);
     }
+
+    public void xulycong(View view) {
+
+        // lấy dữ liệu từ 2 điều khiển
+        String so_1 = editText1.getText().toString();
+        String so_2 = editText2.getText().toString();
+        //chuyển dữ liệu từ chuỗi sang số
+        float so_a = Float.parseFloat(so_1);
+        float so_b = Float.parseFloat(so_2);
+        // tính toán
+        float tong = so_a+so_b;
+        //chuẩn bị dữ liệu
+        String chuoikq = String.valueOf(tong);
+        //gắn kết quả
+        editTextkq.setText(chuoikq);
+    }
+
+    public void xulytru(View view) {
+// lấy dữ liệu từ 2 điều khiển
+        String so_1 = editText1.getText().toString();
+        String so_2 = editText2.getText().toString();
+        //chuyển dữ liệu từ chuỗi sang số
+        float so_a = Float.parseFloat(so_1);
+        float so_b = Float.parseFloat(so_2);
+        // tính toán
+        float hieu = so_a-so_b;
+        //chuẩn bị dữ liệu
+        String chuoikq = String.valueOf(hieu);
+        //gắn kết quả
+        editTextkq.setText(chuoikq);
+
+    }
+
+    public void xulynhan(View view) {
+        // lấy dữ liệu từ 2 điều khiển
+        String so_1 = editText1.getText().toString();
+        String so_2 = editText2.getText().toString();
+        //chuyển dữ liệu từ chuỗi sang số
+        float so_a = Float.parseFloat(so_1);
+        float so_b = Float.parseFloat(so_2);
+        // tính toán
+        float tich = so_a*so_b;
+        //chuẩn bị dữ liệu
+        String chuoikq = String.valueOf(tich);
+        //gắn kết quả
+        editTextkq.setText(chuoikq);
+    }
+
+    public void xulychia(View view) {
+        // lấy dữ liệu từ 2 điều khiển
+        String so_1 = editText1.getText().toString();
+        String so_2 = editText2.getText().toString();
+        //chuyển dữ liệu từ chuỗi sang số
+        float so_a = Float.parseFloat(so_1);
+        float so_b = Float.parseFloat(so_2);
+        // tính toán
+        float thuong = so_a/so_b;
+        //chuẩn bị dữ liệu
+        String chuoikq = String.valueOf(thuong);
+        //gắn kết quả
+        editTextkq.setText(chuoikq);
+    }
+}
