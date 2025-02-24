e9d2c87916860639fa2510251c0bc5b616527abb
Lukari <lukari87@gmail.com>
2025-02-24 20:04:53 +0700

    code main

--- Ex4_AddSubMulDiv_OnClick/app/src/main/java/tiil/edu/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_OnClick/app/src/main/java/tiil/edu/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -1,24 +1,30 @@
-package tiil.edu.ex4_addsubmuldiv_onclick;
+package cntt.edu.ex4_addsubmuldiv;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    //khia bao cac doi tuong gan voi dieu khien tuong ung
+    EditText editText1;
+    EditText editText2;
+    EditText editTextkq;
+    Button nutcong, nuttru, nutnhan, nutchia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        timdieukhien();
     }
-}
+    void timdieukhien(){
+        editText1 = findViewById(R.id.edtsomot);
+        editText2 = findViewById(R.id.edtsohai);
+        editTextkq = findViewById(R.id.edtkq);
+        nutcong = (Button) findViewById(R.id.btncong);
+        nuttru = (Button) findViewById(R.id.btntru);
+        nutnhan = (Button) findViewById(R.id.btnnhan);
+        nutchia = (Button) findViewById(R.id.btnchia);
+    }
