d48c252846ff231a7db2f67b9fd98342900eaa15
Lukari <lukari87@gmail.com>
2025-03-09 15:42:22 +0700

    ex6

---
+++ Ex6_IntentDonGian/app/src/main/java/tiil/edu/ex6_intentdongian/MainActivity.java
@@ -0,0 +1,11 @@
+package tiil.edu.ex6_intentdongian;
+
+package ntu.dtthiep.vidu1_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
