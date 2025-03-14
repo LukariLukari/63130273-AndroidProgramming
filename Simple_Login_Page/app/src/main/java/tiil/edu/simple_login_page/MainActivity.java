0576122906c0ffcb3e47fd0d4702d2529ed4fa9b
Lukari <lukari87@gmail.com>
2025-03-14 17:03:48 +0700

    main

--- Simple_Login_Page/app/src/main/java/tiil/edu/simple_login_page/MainActivity.java
+++ Simple_Login_Page/app/src/main/java/tiil/edu/simple_login_page/MainActivity.java
@@ -1,6 +1,10 @@
 package tiil.edu.simple_login_page;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-}
+
+        
