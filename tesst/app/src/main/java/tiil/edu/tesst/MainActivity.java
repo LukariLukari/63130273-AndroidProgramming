f5e1f8a0b1018b1b9158599e3851b79855844d7b
Lukari <lukari87@gmail.com>
2025-03-18 08:14:32 +0700

    BÀN PHÍM SỐ

--- tesst/app/src/main/java/tiil/edu/tesst/MainActivity.java
+++ tesst/app/src/main/java/tiil/edu/tesst/MainActivity.java
@@ -1,24 +1,54 @@
 package tiil.edu.tesst;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editText = findViewById(R.id.editText);
+
+
+        setNumberButtonListener(R.id.btn1, "1");
+        setNumberButtonListener(R.id.btn2, "2");
+        setNumberButtonListener(R.id.btn3, "3");
+        setNumberButtonListener(R.id.btn4, "4");
+        setNumberButtonListener(R.id.btn5, "5");
+        setNumberButtonListener(R.id.btn6, "6");
+        setNumberButtonListener(R.id.btn7, "7");
+        setNumberButtonListener(R.id.btn8, "8");
+        setNumberButtonListener(R.id.btn9, "9");
+
+
+        Button btnDelete = findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentText = editText.getText().toString();
+                if (!currentText.isEmpty()) {
+                    editText.setText(currentText.substring(0, currentText.length() - 1));
+                }
+            }
+        });
+    }
+
+    private void setNumberButtonListener(int buttonId, final String number) {
+        Button button = findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number);
+            }
         });
     }
 }
