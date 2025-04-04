d586c3e59b57a009a8cb40df2d10ac208ef087f3
Lukari <lukari87@gmail.com>
2025-04-04 13:48:33 +0700

    Update ChucNang2Activity.java

--- thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang2Activity.java
+++ thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang2Activity.java
@@ -5,8 +5,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-public class ActivityChucNang2 extends AppCompatActivity {
 
+public class ActivityChucNang2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,3 +20,15 @@ public class ActivityChucNang2 extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        // Ánh xạ
+        Button btnKiemTra = findViewById(R.id.btnKiemTra);
+        TextView tvKetQua = findViewById(R.id.tvKetQua);
+
+        // Xử lý nút Kiểm tra Đúng/Sai
+        btnKiemTra.setOnClickListener(v -> {
+            // Giả sử thông tin là sai (có thể thay đổi logic kiểm tra)
+            tvKetQua.setText("Thông tin: Sai");
+        });
+    }
+}
