0d80cf141859250e9ed6d20a11347e8229c8305c
Lukari <lukari87@gmail.com>
2025-04-04 14:03:14 +0700

    Update ActivityKyNiem.java

--- thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ActivityKyNiem.java
+++ thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ActivityKyNiem.java
@@ -1,4 +1,19 @@
 package thigk2.phamngocduy;
 
 public class ActivityKyNiem {
+    private String name;
+    private int imageResId;
+
+    public ActivityKyNiem(String name, int imageResId) {
+        this.name = name;
+        this.imageResId = imageResId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
 }
