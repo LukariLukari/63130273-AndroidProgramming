d1e06abb524c1f8a83f5fa6cfd878c39fc81f65a
Lukari <lukari87@gmail.com>
2025-04-04 13:47:35 +0700

    Update ChucNang2Activity.java

--- thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang2Activity.java
+++ thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang2Activity.java
@@ -1,4 +1,9 @@
 package thigk2.phamngocduy;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 public class ChucNang2Activity {
 }
