b3357f0b5043767e4c32155f7b4134567422360e
Lukari <lukari87@gmail.com>
2025-04-04 14:12:03 +0700

    Thông tin cá nhân

--- thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/AboutMeActivity.java
+++ thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/AboutMeActivity.java
@@ -1,4 +1,23 @@
 package thigk2.phamngocduy;
 
-public class AboutMeActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class ActivityAboutMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+
+        // Thiết lập Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("About Me");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+    }
 }
