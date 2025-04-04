b7a383bb78c7d1463cbfc36a1051e8a5daccd27f
Lukari <lukari87@gmail.com>
2025-04-04 13:47:46 +0700

    Update ChucNang2Activity.java

--- thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang2Activity.java
+++ thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang2Activity.java
@@ -5,5 +5,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-public class ChucNang2Activity {
-}
+public class ActivityChucNang2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chuc_nang2);
+
+        // Thiết lập Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Chức năng 2");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
