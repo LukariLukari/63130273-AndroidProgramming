80303991b52d100a0defcbade711f2dd7609067c
Lukari <lukari87@gmail.com>
2025-04-04 13:59:52 +0700

    Update Item3Activity.java

--- thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/Item3Activity.java
+++ thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/Item3Activity.java
@@ -1,4 +1,34 @@
 package thigk2.phamngocduy;
 
-public class Item3Activity {
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Item3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item3);
+
+        // Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Item 3 Activity");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        TextView tvItem = findViewById(R.id.tvItem);
+
+        // Lấy dl
+        String item = getIntent().getStringExtra("item");
+        if (item != null) {
+            tvItem.setText(item);
+            Toast.makeText(this, "Bạn đã chọn: " + item, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
