0b31faca64b9db15673e215d04a9944e14375c7e
Lukari <lukari87@gmail.com>
2025-04-04 13:52:41 +0700

    Update ChucNang3Activity.java

--- thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang3Activity.java
+++ thigk2PhamNgocDuy/app/src/main/java/thigk2/phamngocduy/ChucNang3Activity.java
@@ -1,4 +1,15 @@
 package thigk2.phamngocduy;
 
-public class ChucNang3Activity {
-}
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class ActivityChucNang3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chuc_nang3);
