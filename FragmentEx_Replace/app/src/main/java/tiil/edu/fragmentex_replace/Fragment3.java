73294be97168fe0edcdc38c5760de6cc16fd836a
Lukari <lukari87@gmail.com>
2025-04-18 00:27:26 +0700

    Relace ex3

--- FragmentEx_Replace/app/src/main/java/tiil/edu/fragmentex_replace/Fragment3.java
+++ FragmentEx_Replace/app/src/main/java/tiil/edu/fragmentex_replace/Fragment3.java
@@ -10,29 +10,6 @@ import android.view.ViewGroup;
 
 public class Fragment3 extends Fragment {
 
-
-    public Fragment3() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment3.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment3 newInstance(String param1, String param2) {
-        Fragment3 fragment = new Fragment3();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
